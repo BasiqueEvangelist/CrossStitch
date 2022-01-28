740081da7c02ce8fed842255425aecaaaf7a0836
Basique Evangelist <basiqueevangelist@yandex.ru>
2022-01-28 22:26:07 +0300

    update everything tm

--- src/main/java/com/velocitypowered/crossstitch/mixin/command/CommandTreeSerializationMixin.java
+++ src/main/java/com/velocitypowered/crossstitch/mixin/command/CommandTreeSerializationMixin.java
@@ -11,17 +11,21 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(CommandTreeS2CPacket.class)
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class CommandTreeSerializationMixin {
     private static final Identifier MOD_ARGUMENT_INDICATOR = new Identifier("crossstitch:mod_argument");
 
+    private static final Identifier TEST_ARGUMENT_TYPE = new Identifier("minecraft:test_argument");
+    private static final Identifier TEST_CLASS_TYPE = new Identifier("minecraft:test_class");
+
     @Redirect(method = "writeNode", at = @At(value = "INVOKE", target = "Lnet/minecraft/command/argument/ArgumentTypes;toPacket(Lnet/minecraft/network/PacketByteBuf;Lcom/mojang/brigadier/arguments/ArgumentType;)V"))
     private static void writeNode$wrapInVelocityModArgument(PacketByteBuf packetByteBuf, ArgumentType<?> type) {
-        ArgumentTypes.Entry entry = ArgumentTypes.classMap.get(type.getClass());
+        ArgumentTypes.Entry entry = ArgumentTypes.CLASS_MAP.get(type.getClass());
         if (entry == null) {
             packetByteBuf.writeIdentifier(new Identifier(""));
             return;
         }
-        if (entry.id.getNamespace().equals("minecraft") || entry.id.getNamespace().equals("brigadier")) {
+        if ((entry.id.getNamespace().equals("minecraft") || entry.id.getNamespace().equals("brigadier")) && !TEST_ARGUMENT_TYPE.equals(entry.id) && !TEST_CLASS_TYPE.equals(entry.id)) {
             packetByteBuf.writeIdentifier(entry.id);
             entry.serializer.toPacket(type, packetByteBuf);
             return;
